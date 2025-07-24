781c3f988c807c9ba0a99bee9fd9af8e5b824454
Pear277 <pearlybhalani@gmail.com>
2025-07-24 13:19:28 +0100

    llm added

--- backend/agents/agents.py
+++ backend/agents/agents.py
@@ -1,7 +1,17 @@
 from crewai import Agent
 from backend.tools.anomaly_tools import rule_anomaly_tool, ml_anomaly_tool, combined_anomaly_detector
 from backend.tools.autofix_tool import auto_fix_tool
+from crewai import LLM
+import os
 
+llm = LLM(
+    provider="huggingface",
+    model="huggingface/google/flan-t5-base",
+    temperature=0.1,
+    max_tokens= 512,
+    huggingfacehub_api_token= os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    
+)
 
 auto_fix_agent = Agent(
     name = "FixerAgent",
@@ -9,6 +19,7 @@ auto_fix_agent = Agent(
     goal = "Fix formatting issues and incorrect addresses",
     backstory = "You are an expert in cleaning and correcting billing data, focusing on date formats and address typos.",
     tools = [auto_fix_tool],
+    llm=llm,
     verbose = True
 )
 
@@ -18,5 +29,6 @@ investigator_agent = Agent(
     goal="Find anomalies in billing data using both ML and rule-based logic",
     backstory="You are an expert in detecting anomalies in billing data, using both machine learning and rule-based methods.",
     tools=[combined_anomaly_detector],
+    llm=llm,
     verbose=True
 )
